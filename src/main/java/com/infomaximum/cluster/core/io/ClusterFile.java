868e29058fe3727372b83aaec5f5a1dc1ff844d3
Vladimir A. Ulitin <ulitin@infomaximum.com>
2018-01-11 10:24:24 +0300

    refactoring cluster file increment version: 0.0.42

--- src/main/java/com/infomaximum/cluster/core/io/ClusterFile.java
+++ src/main/java/com/infomaximum/cluster/core/io/ClusterFile.java
@@ -15,7 +15,6 @@ import java.nio.file.Paths;
  */
 public class ClusterFile {
 
-    private static String SCHEME_CLUSTER_FILE = "cfile";
     private static String SCHEME_FILE = "file";
 
     private final Component component;
@@ -26,7 +25,7 @@ public class ClusterFile {
 
         this.uri = uri;
         if (uri.getScheme() == null) throw new RuntimeException("Scheme is null, uri: " + uri.toString());
-        if (!uri.getScheme().equals(SCHEME_FILE) && !uri.getScheme().equals(SCHEME_CLUSTER_FILE)) {
+        if (!uri.getScheme().equals(SCHEME_FILE) && !uri.getScheme().equals(URIClusterFile.SCHEME_CLUSTER_FILE)) {
             throw new RuntimeException("Scheme is not support, uri: " + uri.toString() + ", scheme: " + uri.getScheme());
         }
     }
@@ -35,9 +34,9 @@ public class ClusterFile {
         if (isLocalFile()) {
             Files.copy(Paths.get(uri), file);
         } else {
-            //TODO Ulitin V. Необходимо переписать на поточную обработку, сейчас есть большие накладные расходы на оперативку
-            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(getPathComponentKey(), RControllerClusterFile.class);
-            byte[] content = controllerClusterFile.getContent(getPathFileUUID());
+            //TODO Ulitin V. Необходимо подумать как переписать на поточную обработку, сейчас есть будут большие накладные расходы на оперативку
+            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(URIClusterFile.getPathToComponentKey(uri), RControllerClusterFile.class);
+            byte[] content = controllerClusterFile.getContent(URIClusterFile.getPathToFileUUID(uri));
             Files.write(file, content);
         }
     }
@@ -46,8 +45,8 @@ public class ClusterFile {
         if (isLocalFile()) {
             Files.delete(Paths.get(uri));
         } else {
-            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(getPathComponentKey(), RControllerClusterFile.class);
-            controllerClusterFile.delete(getPathFileUUID());
+            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(URIClusterFile.getPathToComponentKey(uri), RControllerClusterFile.class);
+            controllerClusterFile.delete(URIClusterFile.getPathToFileUUID(uri));
         }
     }
 
@@ -55,8 +54,8 @@ public class ClusterFile {
         if (isLocalFile()) {
             Files.deleteIfExists(Paths.get(uri));
         } else {
-            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(getPathComponentKey(), RControllerClusterFile.class);
-            controllerClusterFile.deleteIfExists(getPathFileUUID());
+            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(URIClusterFile.getPathToComponentKey(uri), RControllerClusterFile.class);
+            controllerClusterFile.deleteIfExists(URIClusterFile.getPathToFileUUID(uri));
         }
     }
 
@@ -73,8 +72,8 @@ public class ClusterFile {
         if (isLocalFile()) {
             return Files.size(Paths.get(uri));
         } else {
-            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(getPathComponentKey(), RControllerClusterFile.class);
-            return controllerClusterFile.getSize(getPathFileUUID());
+            RControllerClusterFile controllerClusterFile = component.getRemotes().getFromSSKey(URIClusterFile.getPathToComponentKey(uri), RControllerClusterFile.class);
+            return controllerClusterFile.getSize(URIClusterFile.getPathToFileUUID(uri));
         }
     }
 
@@ -82,13 +81,4 @@ public class ClusterFile {
         return SCHEME_FILE.equals(uri.getScheme());
     }
 
-    private String getPathComponentKey() {
-        if (!SCHEME_CLUSTER_FILE.equals(uri.getScheme())) throw new RuntimeException("Not support scheme");
-        return uri.getSchemeSpecificPart().split("/")[0];
-    }
-
-    private String getPathFileUUID() {
-        if (!SCHEME_CLUSTER_FILE.equals(uri.getScheme())) throw new RuntimeException("Not support scheme");
-        return uri.getSchemeSpecificPart().split("/")[1];
-    }
 }
