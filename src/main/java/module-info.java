d47e927d3c8b97daa8e0caafa7eba08c5807f681
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-05-10 08:32:32 +0300

    build: 1.1.1p1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,10 @@ module com.infomaximum.cluster {
     requires java.base;
     requires org.slf4j;
     requires org.reflections;
+
+    exports com.infomaximum.cluster.struct;
+    exports com.infomaximum.cluster.core.remote.struct;
+    exports com.infomaximum.cluster.core.remote;
+    exports com.infomaximum.cluster.core.remote.utils.validatorremoteobject;
+    exports com.infomaximum.cluster.utils;
 }
