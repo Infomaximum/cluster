e13c4fad0a820884a1d14f5f88f46b485ea900c9
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-08-04 06:43:22 +0300

    feat: update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,13 @@ module com.infomaximum.cluster {
     exports com.infomaximum.cluster.core.remote;
     exports com.infomaximum.cluster.core.remote.utils.validatorremoteobject;
     exports com.infomaximum.cluster.utils;
+    exports com.infomaximum.cluster;
+    exports com.infomaximum.cluster.core.service.transport.executor;
+    exports com.infomaximum.cluster.exception;
+    exports com.infomaximum.cluster.core.remote.utils;
+    exports com.infomaximum.cluster.anotation;
+    exports com.infomaximum.cluster.core.remote.controller.clusterfile;
+    exports com.infomaximum.cluster.core.io;
+    exports com.infomaximum.cluster.struct.storage;
+    exports com.infomaximum.cluster.core.remote.packer;
 }
