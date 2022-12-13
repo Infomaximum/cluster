54531ef394cc2b678b023beee22d5788ff30f065
Vladimir A. Ulitin <ulitin@infomaximum.com>
2022-12-13 22:12:35 +0300

    feat: add exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -26,5 +26,6 @@ module com.infomaximum.cluster {
     exports com.infomaximum.cluster.core.service.componentuuid;
     exports com.infomaximum.cluster.component.manager;
     exports com.infomaximum.cluster.component.memory;
+    exports com.infomaximum.cluster.component.memory.struct;
     exports com.infomaximum.cluster.component.manager.core;
 }
