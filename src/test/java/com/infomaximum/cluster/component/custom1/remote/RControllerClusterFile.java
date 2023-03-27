223d6655c1d19f5aec385a35151672d4dd688dc1
Vladimir A. Ulitin <v.ulitin@mobin.me>
2023-03-27 20:36:59 +0300

    feat: clear

--- src/test/java/com/infomaximum/cluster/component/custom1/remote/RControllerClusterFile.java
+++
@@ -1,9 +0,0 @@
-package com.infomaximum.cluster.component.custom1.remote;
-
-import com.infomaximum.cluster.core.remote.struct.ClusterInputStream;
-import com.infomaximum.cluster.core.remote.struct.RController;
-
-public interface RControllerClusterFile extends RController {
-
-    ClusterInputStream getInputStream(int size);
-}
