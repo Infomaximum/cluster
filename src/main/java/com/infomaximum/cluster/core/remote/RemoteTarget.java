ee7a31e4e5bf5cbbb4abde220d2596cf4ea602c7
vnaumkin <naumkin.vlad2012@yandex.ru>
2025-06-06 16:41:13 +0300

    refactor: RemoteTarget to Serializable
    Task: IS-614

--- src/main/java/com/infomaximum/cluster/core/remote/RemoteTarget.java
+++ src/main/java/com/infomaximum/cluster/core/remote/RemoteTarget.java
@@ -1,6 +1,7 @@
 package com.infomaximum.cluster.core.remote;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public record RemoteTarget(UUID nodeRuntimeId, int componentId, String componentUuid) {
+public record RemoteTarget(UUID nodeRuntimeId, int componentId, String componentUuid) implements Serializable {
 }
