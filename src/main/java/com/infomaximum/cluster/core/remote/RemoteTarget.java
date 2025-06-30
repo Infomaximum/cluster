eb4f8266cdabfbe995d4a199e9d78dc69c82c9ec
Vlad Naumkin <naumkin.vlad2012@yandex.ru>
2025-06-30 17:40:04 +0300

    refactor: RemoteTarget to RemoteObject (#3)
    * refactor: RemoteTarget to Serializable
    
    Task: IS-614
    
    * refactor: RemoteTarget to RemoteObject
    
    Task: IS-614

--- src/main/java/com/infomaximum/cluster/core/remote/RemoteTarget.java
+++ src/main/java/com/infomaximum/cluster/core/remote/RemoteTarget.java
@@ -1,6 +1,8 @@
 package com.infomaximum.cluster.core.remote;
 
+import com.infomaximum.cluster.core.remote.struct.RemoteObject;
+
 import java.util.UUID;
 
-public record RemoteTarget(UUID nodeRuntimeId, int componentId, String componentUuid) {
+public record RemoteTarget(UUID nodeRuntimeId, int componentId, String componentUuid) implements RemoteObject {
 }
