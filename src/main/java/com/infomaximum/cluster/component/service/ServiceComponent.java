5d0512d47be2efef2e64190c6f8a0f402a57c5be
vnaumkin <naumkin.vlad2012@yandex.ru>
2026-02-05 17:13:08 +0300

    fix: not registered ServiceComponent and MemoryComponent
    Task: IS-734

--- src/main/java/com/infomaximum/cluster/component/service/ServiceComponent.java
+++ src/main/java/com/infomaximum/cluster/component/service/ServiceComponent.java
@@ -1,7 +1,9 @@
 package com.infomaximum.cluster.component.service;
 
+import com.infomaximum.cluster.Cluster;
 import com.infomaximum.cluster.anotation.Info;
 import com.infomaximum.cluster.component.service.internal.service.ClusterInputStreamService;
+import com.infomaximum.cluster.core.service.transport.TransportManager;
 import com.infomaximum.cluster.struct.Component;
 
 @Info(uuid = "com.infomaximum.cluster.component.service")
@@ -14,4 +16,9 @@ public class ServiceComponent extends Component {
         this.clusterInputStreamService = new ClusterInputStreamService();
     }
 
+    @Override
+    public void init(Cluster cluster, TransportManager transportManager) {
+        super.init(cluster, transportManager);
+        start();
+    }
 }
